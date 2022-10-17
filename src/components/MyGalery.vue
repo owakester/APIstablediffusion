ea1ddfa085d3d5705c9a23e72837ecaac74d3ebf
owakester <44281235+owakester@users.noreply.github.com>
2022-10-16 22:39:38 -0300

    cambio placeholer

--- src/components/MyGalery.vue
+++ src/components/MyGalery.vue
@@ -72,9 +72,8 @@ const mouseleave = (dato) => {
 
 <template>
   <div>
-    <h2 class="w-1/2 text-3xl m-2 text-center">
-      Las mejores fotos de stock gratis, imágenes libres de regalías y vídeos
-      compartidos por creadores.
+    <h2 class="  text-3xl mx-36 text-center my-8">
+     Disfruta de esta coleccion de imagenes creadas por Stable Diffusion y compartida a traves de la API de <strong class="text-indigo-500"><a href="http://">lexica.art</a></strong> 
     </h2>
     <label
       for="search"
@@ -105,14 +104,14 @@ const mouseleave = (dato) => {
         v-model.lazy="username"
         type="search"
         id="search"
-        class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class=" block p-4 pl-10 w-full mr-2 text-3xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Buscas fotos realizadas por Stablediffusion"
         required
       />
       <button
         @click="fetchData"
         type="submit"
-        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-white absolute right-2.5 bottom-2.5 bg-indigo-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Search
       </button>
